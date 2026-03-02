f0485bfa33c97cccdc11e7c08b2429b0226b6c03
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-02 15:48:39 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include('config.php'); 
+// ... rest of your code
 $host = 'sql205.infinityfree.com';
 $db_user = 'if0_41282500';
 $db_pass = 'Darkbishop1109'; 
