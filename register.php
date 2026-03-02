29b187a9bec51ec6b1d62db537451bdce537d335
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-02 03:12:10 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,6 @@
-<?php
+<?php ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('config.php');
 
 // Enable error reporting to see exactly what goes wrong
