a6b901a1769a1fea55e845608c8a067e10935b89
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-02 02:35:31 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
 include('config.php');
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = strtoupper($_POST['username']);
     $password = $_POST['password'];
