2adef4660b1360afb8e96c24eda244ae0239a185
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-03 18:38:17 -0500

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="text-align: center; padding: 40px; color: #555; font-size: 0.8rem; font-family: 'Cinzel';">
-        <p>&copy; <?php echo date("Y"); ?> HAZARDOUS WoW. All images &copy; Blizzard Entertainment.</p>
-    </footer>
-</body>
-</html>
