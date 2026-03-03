696d7eb0e8af34d3b3ab9d83207ba0df07fd9e55
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-03 16:43:35 -0500

    Update config.php

--- config.php
+++ config.php
@@ -17,7 +17,14 @@ if ($conn->connect_error) {
 
 // Discord Logging
 $webhook_url = "https://discord.com/api/webhooks/1476721940944388288/BAcRYm0PYlhgfWwuy7QgryZ9JqxHtFkhvrEa7fPSHZGp37nCav32sBzI1acqad1c4sgr";
-
+// Discord Webhook URLs
+$webhooks = [
+    'register' => 'https://discord.com/api/webhooks/1476721940944388288/BAcRYm0PYlhgfWwuy7QgryZ9JqxHtFkhvrEa7fPSHZGp37nCav32sBzI1acqad1c4sgr',
+    'bugs'     => 'https://discord.com/api/webhooks/1476634364090519622/xcPFqB7Qg3Mip--zu_Y0iYsCbJ6XTsj7Q5AVKPSmestbsuBPFdFfG1zjy-tVtsT4N9XY',
+    'level80'  => 'https://discord.com/api/webhooks/1477185331819446384/f2jjOMxl96A4NUH0P_83M0vPJ7kdTUZqbylarW1nffNSVfZbfdheEEe0VB92BFY-YK6a',
+    'updates'  => 'https://discord.com/api/webhooks/1477193815109406865/tBZK0h7y0n_e8PyJQXOEoMAv69WtDtp4DYODdmHmATkH_TjG2BKR2lSGkdcee-VEFBfX',
+    'donations'=> 'https://discord.com/api/webhooks/1478508059247775958/pyTjBzl6NGlkdyomC35FyIp-uB82StKXqBATzYtu7CWjaXIjtpVgP4_8bq2O2TXXbm8Q'
+];
 function sendToDiscord($message) {
     global $webhook_url;
     $data = array('content' => $message);
