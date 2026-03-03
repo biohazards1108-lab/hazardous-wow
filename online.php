14bd264301ae287ec22433b2f27b7d00b630fe47
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-03 18:38:55 -0500

    Delete online.php

--- online.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('config.php');
-$online_query = $conn->query("SELECT COUNT(*) as total FROM $char_db.characters WHERE online = 1");
-$online_data = $online_query->fetch_assoc();
-echo "<h3>Players Online: " . $online_data['total'] . "</h3>";
-?>
