a75b0c21db5998fd791ec5f590f36c20f799d719
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-03 18:38:37 -0500

    Delete header.php

--- header.php
+++
@@ -1,7 +0,0 @@
-<nav class="nav-bar">
-    <div class="nav-container" style="display: flex; justify-content: flex-start; gap: 10px; padding: 10px 20px;">
-        <a href="index.php" class="nav-link">HOME</a>
-        <a href="register.php" class="nav-link">CREATE ACCOUNT</a>
-        <a href="login.php" class="nav-link">LOGIN</a>
-    </div>
-</nav>
