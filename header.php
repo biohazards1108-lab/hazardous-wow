cb051a26236e82c3dd38fb16113f44a0bcadbcd6
biohazards1108-lab <biohazards1108@gmail.com>
2026-03-03 16:34:57 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
 <body>
     <nav class="nav-bar">
         <div class="nav-container">
-            <a href="index.php" class="nav-link gold-link">HOME</a>
-            <a href="register.php" class="nav-link gold-link">CREATE ACCOUNT</a>
-            <a href="login.php" class="nav-link gold-link">LOGIN</a>
+            <a href="index.php" class="nav-link">HOME</a>
+            <a href="register.php" class="nav-link">CREATE ACCOUNT</a>
+            <a href="login.php" class="nav-link">LOGIN</a>
         </div>
     </nav>
